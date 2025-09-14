ad6be7b3bd1504214baee90cce21d90098bd2ac7
Lintao He <lintaohe1106@gmail.com>
2025-09-14 15:40:23 +1000

    Update model path in throughput_test.sh

--- throughput_test.sh
+++ throughput_test.sh
@@ -5,7 +5,7 @@ export HF_ENDPOINT="https://hf-mirror.com"
 uv run -m sglang.bench_serving \
   --backend sglang \
   --host 0.0.0.0 --port 30000 \
-  --model "/data1/DeepSeek-V3.1" \
+  --model deepseek-ai/DeepSeek-V3.1 \
   --dataset-name mmmu \
   --random-input-len 1024 --random-output-len 1024 --random-range-ratio 0 \
   --num-prompts 500 \
